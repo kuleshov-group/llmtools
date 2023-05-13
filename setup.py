6fcf92fe0e99c2c77e38d7dff853df7bdff80cd8
cmsxbc <i@cmsis.me>
2023-05-14 01:34:26 +0800

    chore: make quant_cuda as part of llmtune for better distribution

--- setup.py
+++ setup.py
@@ -5,19 +5,15 @@ setup(
     name='llmtune',
     version='0.1.0',
     packages=find_packages(include=['llmtune', 'llmtune.*']),
+    ext_modules=[cpp_extension.CUDAExtension(
+        'llmtune.engine.quant.quant_cuda',
+        [
+            'llmtune/engine/quant/cuda/quant_cuda.cpp',
+            'llmtune/engine/quant/cuda/quant_cuda_kernel.cu'
+        ]
+    )],
+    cmdclass={'build_ext': cpp_extension.BuildExtension},
     entry_points={
         'console_scripts': ['llmtune=llmtune.run:main']
     }
-)
-
-setup(
-    name='quant_cuda',
-    ext_modules=[cpp_extension.CUDAExtension(
-        'quant_cuda', 
-        [
-        	'llmtune/engine/quant/cuda/quant_cuda.cpp', 
-        	'llmtune/engine/quant/cuda/quant_cuda_kernel.cu'
-        ]
-    )],
-    cmdclass={'build_ext': cpp_extension.BuildExtension}
 )
