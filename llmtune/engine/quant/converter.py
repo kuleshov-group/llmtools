ab3abc9ddf7c36254a724edafc4ac58730c53754
Volodymyr Kuleshov <vol.kuleshov@gmail.com>
2023-06-09 14:56:25 -0400

    udpate converter that passes is_cuda

--- llmtune/engine/quant/converter.py
+++ llmtune/engine/quant/converter.py
@@ -1,19 +1,9 @@
 from llmtune.engine.quant.modules import QuantLinear
 
-# def make_quant(module, names, bits, name=''):
-#     if isinstance(module, QuantLinear):
-#         return
-#     for attr in dir(module):
-#         tmp = getattr(module, attr)
-#         name1 = name + '.' + attr if name != '' else attr
-#         if name1 in names:
-#             setattr(
-#                 module, attr, QuantLinear(bits, tmp.in_features, tmp.out_features)
-#             )
-#     for name1, child in module.named_children():
-#         make_quant(child, names, bits, name + '.' + name1 if name != '' else name1)
-
-def make_quant(module, names, bits, name='', groupsize=-1):
+def make_quant(
+    module, names, bits, name='', groupsize=-1, 
+    is_cuda=True, kernel_switch_threshold=128,
+):
     if isinstance(module, QuantLinear):
         return
     for attr in dir(module):
@@ -27,8 +17,8 @@ def make_quant(module, names, bits, name='', groupsize=-1):
                     in_features=tmp.in_features, 
                     out_features=tmp.out_features, 
                     bias=tmp.bias, 
-                    kernel_switch_threshold=128, 
-                    is_cuda=True
+                    kernel_switch_threshold=kernel_switch_threshold,
+                    is_cuda=is_cuda,
                 )
             )
     for name1, child in module.named_children():
