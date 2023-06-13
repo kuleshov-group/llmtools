9cc2789337b4ca99c79a15e81311b393482d4c7e
Volodymyr Kuleshov <vol.kuleshov@gmail.com>
2023-06-13 10:35:33 -0400

    improved multigpu support

--- llmtune/llms/llama/model.py
+++ llmtune/llms/llama/model.py
@@ -36,7 +36,11 @@ def load_llama(llm_config, checkpoint):
                 del layers[name]
         make_quant(model, layers, llm_config.bits)
     model = accelerate.load_checkpoint_and_dispatch(
-        model=model, checkpoint=checkpoint, device_map='auto'
+            model=model,
+            checkpoint=checkpoint,
+            device_map="auto",
+            # device_map={'': 0},
+            no_split_module_classes=["LlamaDecoderLayer"]
     )
     model.seqlen = 2048
 
