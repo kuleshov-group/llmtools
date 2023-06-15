59ab6e3661bfcb60ec837d253f80705952540e1b
Pclanglais <42674826+Pclanglais@users.noreply.github.com>
2023-06-15 19:40:32 +0200

    Update executor.py
    correct import error

--- llmtune/engine/quant/algorithm/executor.py
+++ llmtune/engine/quant/algorithm/executor.py
@@ -1,4 +1,4 @@
-from llmtune.engine.quant.algorithm.gpq import GPTQ
+from llmtune.engine.quant.algorithm.gptq import GPTQ
 from llmtune.engine.quant.algorithm.quantizer import Quantizer
 
 @torch.no_grad()
