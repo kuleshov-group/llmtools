ef95688b72e90b423dc35535ca7545bf5fd847d5
Volodymyr Kuleshov <vol.kuleshov@gmail.com>
2023-07-12 19:51:29 -0700

    fix spacing in llmconfig

--- llmtune/llms/config.py
+++ llmtune/llms/config.py
@@ -5,20 +5,20 @@ from enum import Enum
 from transformers.utils.hub import PushToHubMixin, cached_file, create_repo
 
 class ModelType(Enum):
-	LLAMA = 'llama'
-	OPT = 'opt'
+    LLAMA = 'llama'
+    OPT = 'opt'
 
 @dataclass
 class LLMConfig(PushToHubMixin):
-	name: str # model name e.g., llama-65b-4bit-g64
-	model_type: ModelType
-	hf_config_name: str
-	hf_tokenizer_config: str
-	bits: int
-	groupsize: int = -1
+    name: str # model name e.g., llama-65b-4bit-g64
+    model_type: ModelType
+    hf_config_name: str
+    hf_tokenizer_config: str
+    bits: int
+    groupsize: int = -1
 
-	def save_pretrained(self, save_dir: str, **kwargs):
-		config_path = join(save_dir, "llmtune_config.json")
+    def save_pretrained(self, save_dir: str, **kwargs):
+        config_path = join(save_dir, "llmtune_config.json")
         with open(config_path, "w", encoding="utf-8") as f:
             json.dump(self.to_dict(), f, indent=2)
 
@@ -28,7 +28,7 @@ class LLMConfig(PushToHubMixin):
         if os.path.isdir(save_dir): 
             config_path = join(save_dir, config_filename)
         else:
-			config_path = cached_file(save_dir, config_filename)
+            config_path = cached_file(save_dir, config_filename)
         with open(config_path, "r", encoding="utf-8") as f:
             return cls(**json.load(f))
                 
