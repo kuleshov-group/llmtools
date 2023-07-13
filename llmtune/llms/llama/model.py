aa029437bf70e5f3dab3a7bf5a7aa07032681666
Volodymyr Kuleshov <vol.kuleshov@gmail.com>
2023-07-12 19:52:00 -0700

    fix path to converter in model files

--- llmtune/llms/llama/model.py
+++ llmtune/llms/llama/model.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 
 from llmtune.utils import find_layers
-from llmtune.engine.inference.converter import make_quant
+from llmtune.engine.quant.converter import make_quant
 
 def load_llama_unquantized(llm_config):
     import torch
