252e8c48c92adadaaf6bd1869dae63392f75f479
Volodymyr Kuleshov <vol.kuleshov@gmail.com>
2023-07-20 23:15:23 -0700

    add new config for autollm

--- llmtune/llms/config.py
+++ llmtune/llms/config.py
@@ -1,43 +1,51 @@
 import os
 import json
-from dataclasses import dataclass
 from enum import Enum
-from transformers.utils.hub import PushToHubMixin, cached_file, create_repo
+from transformers import PretrainedConfig, AutoConfig
+from transformers.utils.hub import PushToHubMixin, cached_file
+from llmtune.engine.quant.config import QuantConfig
 
-class ModelType(Enum):
+class LLMType(Enum):
     LLAMA = 'llama'
     OPT = 'opt'
 
-@dataclass
-class LLMConfig(PushToHubMixin):
-    name: str # model name e.g., llama-65b-4bit-g64
-    model_type: ModelType
-    hf_config_name: str
-    hf_tokenizer_config: str
-    bits: int
-    groupsize: int = -1
+class AutoLLMConfig(PretrainedConfig,PushToHubMixin):
+    def __init__(
+        self, 
+        base_config: PretrainedConfig, 
+        quant_config: QuantConfig = None
+    ):
+        self.base_config = base_config
+        self.quant_config = None
+        if quant_config is not None:
+            self.quant_config = quant_config
+
+    @property
+    def model_type(self):
+        return self.base_config.model_type
 
     def save_pretrained(self, save_dir: str, **kwargs):
-        config_path = join(save_dir, "llmtune_config.json")
-        with open(config_path, "w", encoding="utf-8") as f:
-            json.dump(self.to_dict(), f, indent=2)
+        self.base_config.save_pretrained(save_dir, **kwargs)
+        if self.quant_config is not None:
+            self.quant_config.save_pretrained(save_dir, **kwargs)
 
     @classmethod
-    def from_pretrained(cls, save_dir: str, **kwargs):
-        config_filename = "llmtune_config.json"
-        if os.path.isdir(save_dir): 
-            config_path = join(save_dir, config_filename)
-        else:
-            config_path = cached_file(save_dir, config_filename)
-        with open(config_path, "r", encoding="utf-8") as f:
-            return cls(**json.load(f))
-                
-    def to_dict(self):
-        return {
-            "name": self.name,
-            "model_type": self.model_type,
-            "hf_config_name": self.hf_config_name,
-            "hf_tokenizer_config": self.hf_tokenizer_config,
-            "bits": self.bits,
-            "groupsize": self.groupsize,
-        }
+    def from_pretrained(cls, save_dir: str):
+        # load config
+        base_config = AutoConfig.from_pretrained(save_dir)
+
+        # check if quantized model and config are available
+        try:
+            quant_config = (
+                QuantConfig.from_pretrained(save_dir)
+            )
+        except:
+            quant_config = None
+
+        # check if it's a valid model
+        if base_config.model_type not in [e.value for e in LLMType]:
+            raise NotImplementedError(
+                f"Model type {base_config.model_type} currently not supported"
+            )
+
+        return cls(base_config, quant_config)
