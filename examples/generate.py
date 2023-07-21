9103703c920275ac9b3df172497f1d5615eb6746
Volodymyr Kuleshov <vol.kuleshov@gmail.com>
2023-07-20 22:00:39 -0700

    add generation example for new interface

---
+++ examples/generate.py
@@ -0,0 +1,43 @@
+from transformers import AutoTokenizer
+from llmtune.llms.autollm import AutoLLMForCausalLM
+from llmtune.utils import to_half_precision
+
+# model config
+model_name = './llama-7b-quantized' # can generate this via quantize.py
+DEV = 'cuda'
+
+# load model
+llm = AutoLLMForCausalLM.from_pretrained(model_name)
+llm.eval()
+llm.to(DEV)
+llm = to_half_precision(llm)
+
+# load tokenizer
+tokenizer = AutoTokenizer.from_pretrained('huggyllama/llama-13b')
+
+# encode prompt
+prompt = 'The pyramids were built by'
+input_ids = tokenizer.encode(prompt, return_tensors="pt").to(DEV)
+
+# generation config
+min_length=10
+max_length=200
+top_p=.95
+top_k=25
+temperature=1.0
+
+# generate text
+with torch.no_grad():
+    generated_ids = llm.generate(
+        inputs=input_ids,
+        do_sample=True,
+        min_length=min_length,
+        max_length=max_length,
+        top_p=top_p,
+        top_k=top_k,
+        temperature=temperature,
+    )
+
+# decode and print
+output = tokenizer.decode([el.item() for el in generated_ids[0]])
+print(output)
