de569ba1234b758ff7e6e5d423c3d1c5ae2e2e17
Volodymyr Kuleshov <vol.kuleshov@gmail.com>
2023-08-30 15:17:26 -0400

    add bloom quantization code

--- llmtune/engine/quant/gptq/executor.py
+++ llmtune/engine/quant/gptq/executor.py
@@ -26,6 +26,17 @@ class GPTQAlgorithm(QuantizationAlgorithm):
                 percdamp=self.config.percdamp,
                 nearest=self.config.nearest,
             )
+        elif model.config.model_type == LLMType.BLOOM.value:
+            quantizers = quantize_bloom(
+                model.base_model,
+                dataloader,
+                bits=self.config.bits,
+                groupsize=self.config.groupsize,
+                act_order=self.config.act_order,
+                nsamples=self.config.nsamples,
+                percdamp=self.config.percdamp,
+                nearest=self.config.nearest,
+            )
         else:
             raise NotImplementedError(
                 f'{model.config.model_type} not supported'
@@ -33,7 +44,7 @@ class GPTQAlgorithm(QuantizationAlgorithm):
         print(f'Quantization time (s): {time.time() - tick}')
 
         # pack the weights according to the quantizers
-        pack_llama(model.base_model, quantizers, self.config.bits, self.config.groupsize)
+        pack_weights(model.base_model, quantizers, self.config.bits, self.config.groupsize)
 
         # save quantization config
         model.set_quant_config(self.config)
@@ -156,7 +167,103 @@ def quantize_llama(
     
     return quantizers
 
-def pack_llama(model, quantizers, wbits, groupsize):
+@torch.no_grad()
+def quantize_bloom(
+    model, dataloader, bits, groupsize, act_order, nsamples, percdamp, 
+    sym=False, true_sequential=False, nearest=False, dev='cuda'
+):
+    print('Starting ...')
+
+    use_cache = model.config.use_cache
+    model.config.use_cache = False
+    layers = model.transformer.h
+
+    model.transformer.word_embeddings = model.transformer.word_embeddings.to(dev)
+    model.transformer.word_embeddings_layernorm = model.transformer.word_embeddings_layernorm.to(dev)
+    layers[0] = layers[0].to(dev)
+
+    dtype = next(iter(model.parameters())).dtype
+    inps = torch.zeros(
+        (nsamples, model.seqlen, model.config.hidden_size), dtype=dtype, device=dev
+    )
+    cache = {'i': 0, 'attention_mask': None, 'alibi': None}
+
+    class Catcher(nn.Module):
+        def __init__(self, module):
+            super().__init__()
+            self.module = module
+        def forward(self, inp, **kwargs):
+            inps[cache['i']] = inp
+            cache['i'] += 1
+            cache['attention_mask'] = kwargs['attention_mask']
+            cache['alibi'] = kwargs['alibi']
+            raise ValueError
+    layers[0] = Catcher(layers[0])
+    for batch in dataloader:
+        try:
+            model(batch[0].to(dev))
+        except ValueError:
+            pass
+    layers[0] = layers[0].module
+
+    layers[0] = layers[0].cpu()
+    model.transformer.word_embeddings = model.transformer.word_embeddings.cpu()
+    model.transformer.word_embeddings_layernorm = model.transformer.word_embeddings_layernorm.cpu()
+    torch.cuda.empty_cache()
+
+    outs = torch.zeros_like(inps)
+    attention_mask = cache['attention_mask']
+    alibi = cache['alibi']
+
+    print('Ready.')
+
+    quantizers = {}
+    for i in range(len(layers)):
+        layer = layers[i].to(dev)
+
+        subset = find_layers(layer)
+        gptq = {}
+        for name in subset:
+            gptq[name] = GPTQ(subset[name])
+            gptq[name].quantizer = Quantizer()
+            gptq[name].quantizer.configure(
+                bits, perchannel=True, sym=False, mse=False
+            )
+
+        def add_batch(name):
+            def tmp(_, inp, out):
+                gptq[name].add_batch(inp[0].data, out.data)
+            return tmp
+        handles = []
+        for name in subset:
+            handles.append(subset[name].register_forward_hook(add_batch(name)))
+        for j in range(nsamples):
+            outs[j] = layer(inps[j].unsqueeze(0), attention_mask=attention_mask, alibi=alibi)[0]
+        for h in handles:
+            h.remove()
+
+        for name in subset:
+            print(i, name)
+            print('Quantizing ...')
+            if not nearest:
+                scale,zero,g_idx = gptq[name].fasterquant(percdamp=percdamp, groupsize=groupsize, actorder=act_order)
+                quantizers['transformer.h.%d.%s' % (i, name)] = (gptq[name].quantizer.cpu(),scale.cpu(),zero.cpu(),g_idx.cpu())
+                gptq[name].free()
+            else:
+                raise RuntimeError('Nearest neighbor BLOOM quantization not supported')
+        for j in range(nsamples):
+            outs[j] = layer(inps[j].unsqueeze(0), attention_mask=attention_mask, alibi=alibi)[0]
+
+        layers[i] = layer.cpu()
+        del gptq 
+        torch.cuda.empty_cache()
+
+        inps, outs = outs, inps
+
+    model.config.use_cache = use_cache  
+    return quantizers  
+
+def pack_weights(model, quantizers, wbits, groupsize):
     layers = find_layers(model)
     layers = {n: layers[n] for n in quantizers}
     make_quant(model, quantizers, wbits, groupsize)
