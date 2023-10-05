5a332ba1a2a3c9fdae500cbf8498327fd9be3233
oscarscaro <32592746+oscarscaro@users.noreply.github.com>
2023-10-05 13:47:48 -0400

    Merge issues fixed: quant_cuda set up

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup, find_packages, Extension
 from torch.utils import cpp_extension
 
 setup(
-    name='llmtune',
+    name='llmtools',
     version='0.1.0',
-    packages=find_packages(include=['llmtune', 'llmtune.*']),
+    packages=find_packages(include=['llmtools', 'llmtools.*']),
     entry_points={
-        'console_scripts': ['llmtune=llmtune.run:main']
+        'console_scripts': ['llmtools=llmtools.run:main']
     }
 )
 
@@ -15,8 +15,8 @@ setup(
     ext_modules=[cpp_extension.CUDAExtension(
         'quant_cuda', 
         [
-        	'llmtune/engine/quant/cuda/quant_cuda.cpp', 
-        	'llmtune/engine/quant/cuda/quant_cuda_kernel.cu'
+        	'llmtools/engine/inference/cuda/quant_cuda.cpp', 
+        	'llmtools/engine/inference/cuda/quant_cuda_kernel.cu'
         ]
     )],
     cmdclass={'build_ext': cpp_extension.BuildExtension}
